450642d8e8d5cb0cfe3b49b7be9e8676c838a0cf
Per Sundberg (Polystar) <per.sundberg@polystar.com>
2017-03-01 21:55:29 +0100

    Fia full board complete

--- main.js
+++ main.js
@@ -34,25 +34,159 @@ $(document).ready(function(){
   function drawBgCircles() {
     var canvas = document.getElementById('canvas1');
 
-    for (var topPos = 1; topPos<=11; topPos++) {
+    for (var topPos = 1; topPos<=57; topPos++) {
       var context = canvas.getContext('2d');
-      var centerX = 44*topPos;
-      var centerY = canvas.height / 2;
+      var centerX = 0;
+      var centerY = 0;
       var radius = 20;
+
+      // Bottom left to up
+      if (topPos<6) {
+        centerX = 300 - 44;
+        centerY = 600 - 44*topPos;
+      }
+
+      //Middle left to right
+      if (topPos>5 && topPos<10) {
+        centerX = 300 - (44* (topPos-4));
+        centerY = 600 - 44*5;
+      }
+
+      //middle left
+      if (topPos === 10) {
+        centerX = 300 - (44*5);
+        centerY = 600 - 44*6;
+      }
+
+      //middle left upper left to right
+      if (topPos>10 && topPos<16) {
+        centerX = 300 - (44*5) + ((topPos-11) * 44);
+        centerY = 600 - 44*7;
+      }
+
+      //top left bottom to up
+      if (topPos>15 && topPos<20) {
+        centerX = 300 - 44;
+        centerY = 600 - (44* (topPos-8));
+      }
+
+      //top
+      if (topPos === 20) {
+        centerX = 300;
+        centerY = 600 - 44*11;
+      }
+
+      //top right up to bottom
+      if (topPos>20 && topPos<25) {
+        centerX = 300 + 44;
+        centerY = 600 - (44*11) + ((topPos-21) * 44);
+      }
+
+      //middle right upper
+      if (topPos>24 && topPos<30) {
+        centerX = 300 + 44 + ((topPos-25) * 44);
+        centerY = 600 - (7*44);
+      }
+
+      //middle right
+      if (topPos === 30) {
+        centerX = 300 + (44*5);
+        centerY = 600 - 44*6;
+      }
+
+      //Middle left lower left to right
+      if (topPos>30 && topPos<36) {
+        centerX = 300 + (44*5) - (44* (topPos-31));
+        centerY = 600 - 44*5;
+      }
+
+      //bottom right up to down
+      if (topPos>35 && topPos<40) {
+        centerX = 300 + 44;
+        centerY = 600 - (44*4) + ((topPos-36) * 44);
+      }
+
+      //middle bottom
+      if (topPos === 40) {
+        centerX = 300;
+        centerY = 600 - 44;
+      }
+
+      //up to final
+      if (topPos>40  && topPos <45) {
+        centerX = 300;
+        centerY = 600 - 44*(topPos-39);
+      }
+
+      //final point
+      if (topPos === 45) {
+        centerX = 300;
+        centerY = 600 - 44*6;
+      }
+
+      //left middle points
+      if (topPos>45 && topPos<50) {
+        centerX = 300 - (44* (topPos-45));
+        centerY = 600 - 44*6;
+      }
+
+      //upper middle points
+      if (topPos>49 && topPos<54) {
+        centerX = 300;
+        centerY = 600 - (44* (topPos-43));
+      }
+
+      //right middle points
+      if (topPos>53) {
+        centerX = 300 + 44 + ((topPos-54) * 44);
+        centerY = 600 - (6*44);
+      }
 
       context.beginPath();
       context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
 
-      //Add color for each circle
-      if (topPos % 3 === 0) {
-        context.fillStyle = 'green';
+      //Add color for each circle , ecept for in final stage
+      if (topPos <41) {
+        if (topPos % 4 === 0) {
+          context.fillStyle = 'green';
+        }
+        if (topPos % 4 === 1) {
+          context.fillStyle = 'blue';
+        }
+        if (topPos % 4 === 2) {
+          context.fillStyle = 'red';
+        }
+        if (topPos % 4 === 3) {
+          context.fillStyle = 'yellow';
+        }
       }
-      if (topPos % 3 === 1) {
-        context.fillStyle = 'blue';
+
+      //Color for final stage
+      if (topPos>40 && topPos<45) {
+          context.fillStyle = 'blue';
       }
-      if (topPos % 3 === 2) {
-        context.fillStyle = 'red';
+
+
+     //Colof for final position
+      if (topPos === 45) {
+          context.fillStyle = 'white';
       }
+
+     //color for left middle positions
+      if (topPos > 45 && topPos < 50) {
+          context.fillStyle = 'yellow';
+      }
+
+      //color for upper middle positions
+       if (topPos > 49 && topPos <54) {
+           context.fillStyle = 'red';
+       }
+
+       //color for right  middle positions
+        if (topPos > 54 && topPos <58) {
+            context.fillStyle = 'green';
+        }
+
 
       context.fill();
       context.lineWidth = 1;
@@ -70,7 +204,7 @@ $(document).ready(function(){
     context = canvas.getContext('2d');
     if (pos === 0) {
       centerY = (canvas.height / 2) -10;
-    context.fillStyle = top1Color;
+      context.fillStyle = top1Color;
     }
 
     if (pos === 1) {
@@ -92,7 +226,7 @@ $(document).ready(function(){
 
   function displayTopValue(topValue,pos)  {
     if (pos === 0) {
-    //  var toe = document.getElementById('topvalue');
+      //  var toe = document.getElementById('topvalue');
 
       //Draw bgCircles
       drawBgCircles();
@@ -101,11 +235,11 @@ $(document).ready(function(){
 
     }
     if (pos === 1){
-  //    var toe = document.getElementById('topvalue2');
+      //    var toe = document.getElementById('topvalue2');
       //draw top circle
       drawTopCircle(topValue,pos);
     }
-  //  toe.innerText = topValue;
+    //  toe.innerText = topValue;
   }
 
   function checkForCollision(currentTop) {
@@ -124,10 +258,10 @@ $(document).ready(function(){
   function displayGOver(topNo)  {
     var goElement = document.getElementById('gameover');
     if (topNo === 0) {
-       goElement.innerText = 'Game Over, Top 1 won';
+      goElement.innerText = 'Game Over, Top 1 won';
     }
     if (topNo === 1) {
-       goElement.innerText = 'Game Over, Top 2 won';
+      goElement.innerText = 'Game Over, Top 2 won';
     }
   }
 
@@ -215,23 +349,23 @@ $(document).ready(function(){
   var lastpos = posFinished + 1;
   var bgPositions = '';
   for(var i = 0; i<lastpos; i++) {
-    bgPositions += " " + i;
-  }
-  bgElement.innerText = bgPositions;
-  */
+  bgPositions += " " + i;
+}
+bgElement.innerText = bgPositions;
+*/
 
-  //display default top and default top value
-  //displayTop(1,0);      //display top 0 pos 1
-  displayTopValue(1,0); //display top 0 value 1
-  //displayTop(1,1);      //display top 1 pos 1
-  displayTopValue(1,1); //display top 1 value 1
+//display default top and default top value
+//displayTop(1,0);      //display top 0 pos 1
+displayTopValue(1,0); //display top 0 value 1
+//displayTop(1,1);      //display top 1 pos 1
+displayTopValue(1,1); //display top 1 value 1
 
-  var canvas = document.getElementById('canvas1');
+var canvas = document.getElementById('canvas1');
 
-  //Draw bgCircles
-  drawBgCircles();
+//Draw bgCircles
+drawBgCircles();
 
-  //draw top circle for top 1 in pos 1 (default)
-  drawTopCircle(1,0);
-  drawTopCircle(1,1);
+//draw top circle for top 1 in pos 1 (default)
+drawTopCircle(1,0);
+drawTopCircle(1,1);
 });
